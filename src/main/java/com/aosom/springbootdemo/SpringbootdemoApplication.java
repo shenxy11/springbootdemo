5f05aa4919bcce70c74bce1e6ad541ae66b4f911
shenxy11 <329274026@qq.com>
2019-06-19 18:20:08 +0800

    update again3

--- src/main/java/com/aosom/springbootdemo/SpringbootdemoApplication.java
+++ src/main/java/com/aosom/springbootdemo/SpringbootdemoApplication.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class SpringbootdemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringbootdemoApplication.class, args);
-        System.out.println("测试自动化部署main。。。。。。。ffffdfsdfsd。。hello world。。。。");
+        System.out.println("------测试自动化部署main。。。。。。。ffffdfsdfsd。。hello world。。。。");
     }
 
 }
